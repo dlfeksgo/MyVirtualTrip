7699ecb0a9e6ac8ab434821078f6ebc2f439bd89
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-07-22 14:17:35 +0900

    refactor: Header Button class 부여 및 스타일 조정

--- src/components/UI/MyHeader/MyHeader.jsx
+++ src/components/UI/MyHeader/MyHeader.jsx
@@ -1,14 +1,14 @@
+import classnames from 'classnames/bind';
 import React from 'react';
 import styles from './MyHeader.module.css';
-import classnames from 'classnames/bind';
 const cm = classnames.bind(styles);
 
 const MyHeader = ({ value, leftChild, rightChild }) => {
 	return (
 		<div className={cm('wrapper')}>
-			<div>{leftChild}</div>
+			<div className={cm('left')}>{leftChild}</div>
 			<h1>{value}</h1>
-			<div>{rightChild}</div>
+			<div className={cm('right')}>{rightChild}</div>
 		</div>
 	);
 };
