03eb5ab6307c96f5ec7ad39f0cc123012bb27bc6
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-07-22 14:17:45 +0900

    feat: 카테고리 삭제 기능 추가

--- src/components/CheckList/CheckList.jsx
+++ src/components/CheckList/CheckList.jsx
@@ -1,46 +1,44 @@
-import React from 'react';
-import ItemForm from './Form/ItemForm';
-import MyHeader from '../UI/MyHeader/MyHeader';
-import MyButton from '../UI/MyButton/MyButton';
-import { useNavigate, useParams } from 'react-router';
-import { useSelector } from 'react-redux';
 import { createSelector } from '@reduxjs/toolkit';
-import CheckListItem from './CheckListItem/CheckListItem';
-import styles from './CheckList.module.css';
 import classnames from 'classnames/bind';
-import ItemFilter from './Filter/ItemFilter';
-import { selectFilter } from '../../slice/filter';
-import { useCategoryItemList } from '../../hooks/useCategory';
+import React from 'react';
 import { ThreeDots } from 'react-loader-spinner';
+import { useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router';
+import { useCategory, useCategoryItemList } from '../../hooks/useCategory';
+import { selectFilter } from '../../slice/filter';
+import MyButton from '../UI/MyButton/MyButton';
+import MyHeader from '../UI/MyHeader/MyHeader';
+import styles from './CheckList.module.css';
+import CheckListItem from './CheckListItem/CheckListItem';
+import ItemFilter from './Filter/ItemFilter';
+import ItemForm from './Form/ItemForm';
 const cm = classnames.bind(styles);
 
-const selectItemsByCategory = createSelector(
-	[selectFilter, (state, items) => items],
-	(status, items) => {
-		switch (status) {
-			case '전체':
-				return items;
-			case '완료':
-				return items?.filter((item) => item.isCompleted);
-			case '미완료':
-				return items?.filter((item) => !item.isCompleted);
+const selectItemsByCategory = createSelector([selectFilter, (state, items) => items], (status, items) => {
+	switch (status) {
+		case '전체':
+			return items;
+		case '완료':
+			return items?.filter((item) => item.isCompleted);
+		case '미완료':
+			return items?.filter((item) => !item.isCompleted);
 
-			default:
-				return items;
-		}
+		default:
+			return items;
 	}
-);
+});
 
 const CheckList = () => {
-	console.log('CheckList');
 	const { name } = useParams();
 	const navigate = useNavigate();
 	const {
 		itemListQuery: { data: itemList, isLoading, refetch },
 		addItem,
 	} = useCategoryItemList(name);
-
+	const { deleteCategory } = useCategory();
 	const items = useSelector((state) => selectItemsByCategory(state, itemList));
+
+	const handleDelete = () => deleteCategory.mutateAsync(name).then(() => navigate('/'));
 
 	if (isLoading) {
 		return (
@@ -61,19 +59,11 @@ const CheckList = () => {
 		<div className={cm('wrapper')}>
 			<MyHeader
 				value={name}
-				leftChild={
-					<MyButton
-						value={'이전'}
-						type={'default'}
-						onClick={() => navigate(-1)}
-					/>
-				}
+				leftChild={<MyButton value="이전" type="default" onClick={() => navigate(-1)} />}
+				rightChild={<MyButton value="삭제" type="warning" onClick={handleDelete} />}
 			/>
 			<ItemFilter refetch={refetch} />
-			<ul className={cm('list')}>
-				{items &&
-					items.map((item) => <CheckListItem key={item.id} item={item} />)}
-			</ul>
+			<ul className={cm('list')}>{items && items.map((item) => <CheckListItem key={item.id} item={item} />)}</ul>
 			<ItemForm category={name} addItem={addItem} />
 		</div>
 	);
