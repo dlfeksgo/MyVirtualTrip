a94b5ca2e61b688ed93abba5f75625c4afc3d9de
KimJeongHee <93242226+k-jeonghee@users.noreply.github.com>
2024-07-22 14:47:07 +0900

    feat: router basename 추가

--- src/route.js
+++ src/route.js
@@ -1,24 +1,29 @@
 import { createBrowserRouter } from 'react-router-dom';
-import Home from './pages/Home';
 import Create from './pages/Create';
 import Edit from './pages/Edit';
+import Home from './pages/Home';
 import List from './pages/List';
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+	[
+		{
+			path: '/',
+			element: <Home />,
+		},
+		{
+			path: '/info',
+			element: <Create />,
+		},
+		{
+			path: '/info/edit',
+			element: <Edit />,
+		},
+		{
+			path: '/category/:name',
+			element: <List />,
+		},
+	],
 	{
-		path: '/',
-		element: <Home />,
-	},
-	{
-		path: '/info',
-		element: <Create />,
-	},
-	{
-		path: '/info/edit',
-		element: <Edit />,
-	},
-	{
-		path: '/category/:name',
-		element: <List />,
-	},
-]);
+		basename: process.env.PUBLIC_URL,
+	}
+);
